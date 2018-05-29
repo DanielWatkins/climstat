e41314969ba0eb30e83eb1ded5159c4ba7ccd133
Daniel Watkins <danielmwatkins@gmail.com>
2018-05-29 13:01:13 -0700

    Update reading_sheba_data.py

--- reading_sheba_data.py
+++ reading_sheba_data.py
@@ -5,7 +5,8 @@ the name you want the output file to have as 'save_prefix'. Still not perfect - 
 a few files with corrupted data when I tried it, with things like $ instead of 0. I've been
 able to dodge most of those via the 'find_row_range' function but a few still fail to load.
 Soundings that fail to read in have the date printed, everything else is wrapped together and
-saved."""
+saved. When a sounding fails to read, a second attempt is made, leaving out the widths argument.
+This appears to work for now but is not necessarily very robust."""
 
 
 import os
@@ -16,6 +17,42 @@ from datetime import datetime
 drive_loc = 
 save_prefix = 
 min_pressure = 500
+
+def find_row_range(fname, min_pressure):
+    """Scans file to identify beginning, end, and any rows with nonnumerics"""
+    filter_char = lambda char: char.isalnum() or char in ['-', ' ', '.', '\n']
+
+    with open(fname) as f:
+        first = 0
+        last = 0
+        lines = f.readlines()
+        nrows = len(lines)
+        
+        for idx in range(len(lines)):
+            line = lines[idx]
+            if line.split()[0] == '------':
+                first = idx + 1
+                skip_rows = list(np.arange(first))
+            if (idx > first) & (first != 0):
+                if float(line.split()[1]) < min_pressure:
+                    last = int(idx)
+                if not np.all([filter_char(x) for x in line]):
+                    skip_rows.append(idx)
+                    
+        if last == 0:
+            last = nrows
+            
+        if last < nrows:
+            skip_rows += list(np.arange(last, nrows + 1))
+            
+    return skip_rows
+
+                
+
+import os
+import pandas as pd
+import numpy as np
+from datetime import datetime
 
 def find_row_range(fname, min_pressure):
     """Scans file to identify beginning, end, and any rows with nonnumerics"""
@@ -62,7 +99,7 @@ def read_sheba_data(fpath, min_pressure=0):
                 'relative_humidity', 'u_wind', 'v_wind', 'speed',
                 'direction', 'ascent_rate', 'longitude', 'latitude', 
                 'range', 'azimuth_angle', 'height', 'qp', 'qt',
-                'qrh', 'qu', 'qv', 'qdz']
+                'qrh', 'qu', 'qv', 'quv']
 
     na_values = {'time':9999.0, 'pressure':9999.0, 'temperature':999.0, 'dewpoint':999.0,
              'relative_humidity':9999.0, 'u_wind':999.0, 'v_wind':999.0, 'speed':999.0,
@@ -76,6 +113,10 @@ def read_sheba_data(fpath, min_pressure=0):
              'range':float, 'azimuth_angle':float, 'height':float, 'qp':float, 'qt':float,
              'qrh':float, 'qu':float, 'qv':float, 'qdz':float}
     
+    widths = np.array([7, 7, 6, 6, 6, 7, 7,
+                       6, 6, 6, 9, 8, 6, 6,
+                       8, 5, 5, 5, 5, 5, 5])
+    
     # Find line 16. Some files have repeated lines in the header
     with open(fpath) as f:
         for i, line in enumerate(f):
@@ -87,23 +128,22 @@ def read_sheba_data(fpath, min_pressure=0):
     try:
         df = pd.read_fwf(fpath, na_values=na_values,
                          skiprows=skip_rows, dtype=dtype, 
+                         widths=widths,
                          names=colnames)
         df = df[df['pressure'] >= min_pressure].copy()
         df.date = date
         return df
-    except ValueError:
+    except ValueError as e:
         print(date)
-        
-        
+        print(e)
+        try:
+            df = pd.read_fwf(fpath, na_values=na_values,
+                             skiprows=skip_rows, dtype=dtype,
+                             names=colnames)
+            df = df[df['pressure'] >= min_pressure].copy()
+            df.date = date
+            return df
 
-sounding_list = []
-for folder in os.listdir(drive_loc):
-    if folder[0:2] == '19':
-        path = os.path.join(drive_loc, folder)
-        print(path)
-        for file in os.listdir(path):
-            if file[0] is not '.':
-                df = read_sheba_data(os.path.join(path, file), min_pressure)
-                sounding_list.append(df)
-sounding_df = pd.concat(sounding_list)
-sounding_df.to_csv(os.path.join(drive_loc, save_prefix + '.csv'))
+            
+        except ValueError:
+            print('Also failed without widths column')
